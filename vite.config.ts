b287bcfe4dd03f98d2a6ab73ac9f63e6840664e8
fauxsarus <minifyre@gmail.com>
2025-01-30 10:25:27 -0500

    gh-pages autodeploy test

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import solidPlugin from 'vite-plugin-solid'
 export default defineConfig({
 	plugins: [solidPlugin()],
 	server: {
+		/** @ts-ignore following [this tutoria](https://dev.to/lexlohr/deploy-a-solid-start-app-on-github-pages-2l2l) */
+		baseURL: process.env.BASE_PATH,
+		preset: 'static',
 		port: 3000,
-		watch: {
-			usePolling: true,
-		},
+		/** @note WSL-specific */
+		watch: {usePolling: true},
 	},
 	build: {
 		target: 'esnext',
